95ba240de1f5515472e34b8979e1e96f4fd3b025
NilsonKr <nuotrok444@gmail.com>
2021-07-22 00:01:51 -0500

    :tada: [INITIAL] Setup Done

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+			<img src='/images/LigthLogo.png' alt='asdasd' />
+		</div>
+	);
+};
+
+export default Home;
