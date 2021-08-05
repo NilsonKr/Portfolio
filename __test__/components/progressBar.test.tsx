ce03cf92083463913ccd66ee23b37afe6184b054
NilsonKr <nuotrok444@gmail.com>
2021-08-05 03:52:03 -0500

    :test_tube: [ADD] Husky hook for test on pre-push

--- __test__/components/progressBar.test.tsx
+++ __test__/components/progressBar.test.tsx
@@ -17,6 +17,6 @@ describe('<ProgressBar />', () => {
 	test('useScrollBar hook', () => {
 		const { result } = renderHook(() => useScrollBar(25));
 
-		expect(result.current.scroll).toBe(25);
+		expect(result.current.scroll).toBe(205);
 	});
 });
